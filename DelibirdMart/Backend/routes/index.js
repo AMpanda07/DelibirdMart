de038c6411fe0714d1ab060013e5ae003af61e87
Ayushman <ayushmanpanda120@gmail.com>
2026-07-30 18:44:51 +0530

    chore: remove redundant nested DelibirdMart directory

--- DelibirdMart/Backend/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const healthRoutes = require('./health.routes');
-
-const router = express.Router();
-
-// Register module routes under /api/v1
-router.use('/health', healthRoutes);
-
-module.exports = router;
